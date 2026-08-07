23fed81156d8d4548dd3e8edff8e5e915588d525
Debojyoti Kundu <kundudebojyoti87@gmail.com>
2026-08-07 23:52:30 +0530

    Time: 9 ms (54.58%), Space: 11.7 MB (81.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -15,8 +15,9 @@ public:
         while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast)
+            if (slow == fast) {
                 return true;
+            }
         }
         return false;
     }
